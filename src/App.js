6934aa87ab2ad95f8bd866ba1dfb5eaccd7b18a1
Greg <gregu@24data.com>
2019-04-09 09:36:55 -0400

    Render method and getting started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-const App = () => <h1>Hello World</h1>
+// Class component: can have state
+class App extends React.Component {
+    render() {  //('element', props(properties), 'another element or a string representing our element content')
+        // return React.createElement('h1', null, 'Hello Eggheads')
+        return (
+            <div>
+                <h1 className="classIsAReservedKeyName">Hello World</h1>
+                <strong>Bold</strong>
+            </div>
+        )
+    }
+}
 
+// Stateless function component
+// const App = () => <h1>Hello Greg</h1>
 export default App
+
