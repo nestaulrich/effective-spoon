8bb86abf71a20a5d20b19dcd65bf8e8542a06760
Greg <gregu@24data.com>
2019-04-13 18:59:43 -0400

    Bare bones app structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import MainContent from './MainContent';
 
 function App() {
     return(
         <div>
-            <Header />
-            <MainContent />
-            <Footer/>
+           
         </div>
     )
 }
